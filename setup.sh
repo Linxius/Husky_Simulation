62450a77374eed714a8e8306bfe882247f9c0871
Linxius <linique@163.com>
2019-05-06 15:18:02 +0800

    move models directory out of repository

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ export HUSKY_DESCRIPTION=$(rospack find husky_custom_description)/urdf/custom_de
 #export HUSKY_URDF_EXTRAS=$(rospack find husky_description)/urdf/empty.urdf
 export HUSKY_URDF_EXTRAS=$(rospack find husky_custom_description)/urdf/custom_description.urdf.xacro
 #sh $(rospack find husky_custom_gazebo)/env-hooks/50.husky_custom_gazebo.sh
-export GAZEBO_MODEL_PATH=~/ros/Husky_Simulation/src/husky_custom_gazebo/models
+export GAZEBO_MODEL_PATH=~/ros/models
