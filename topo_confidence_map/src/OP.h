089aaad1b25898dd7efd837e90be3d9398a62e6e
Linxius <linique@163.com>
2019-05-18 00:28:42 +0800

    fix bug op.h

--- topo_confidence_map/src/OP.h
+++ topo_confidence_map/src/OP.h
@@ -129,6 +129,8 @@ public:
     void PrintPlanNodes(const int & iQueryIdx,
 	                    const std::vector<ConfidenceValue> & vConfidenceMap);
 
+	std::vector<Node> m_vAllNodes;
+
 private:
 
 	//the grid index of which current robot is 
@@ -143,7 +145,6 @@ private:
     std::vector<int> m_vPastNodeIdxs;//note that index is in m_vAllNodes
 
     //all generated nodes
-	std::vector<Node> m_vAllNodes;
 
 	//branch and bound based method's object
 	BranchBound BBSolver;
